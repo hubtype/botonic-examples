af8ee3b5f9d677206b18e07d250d3ed9d9badd07
Marc Rabat <marcrabat@gmail.com>
2020-12-17 16:44:19 +0100

    chore: correct workflows and analytics

--- blank-typescript/analytics.js
+++ blank-typescript/analytics.js
@@ -1,26 +1,54 @@
+const Analytics = require('analytics-node')
+const fs = require('fs')
 const os = require('os')
+const path = require('path')
 
-function analytics_enabled() {
+const BOTONIC_HOME_PATH = path.join(os.homedir(), '.botonic')
+const BOTONIC_CREDENTIALS_PATH = path.join(
+  BOTONIC_HOME_PATH,
+  'credentials.json'
+)
+
+let credentials
+const analytics = new Analytics('YD0jpJHNGW12uhLNbgB4wbdTRQ4Cy1Zu')
+
+function analyticsEnabled() {
   return process.env.BOTONIC_DISABLE_ANALYTICS !== '1'
 }
 
 function getSystemInformation() {
   return {
     platform: os.platform(),
-    version: os.version(),
     arch: os.arch(),
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
     timestamp: new Date().toISOString(),
   }
 }
 
+function initializeCredentials() {
+  if (!fs.existsSync(BOTONIC_HOME_PATH)) fs.mkdirSync(BOTONIC_HOME_PATH)
+  const anonymous_id = Math.round(Math.random() * 100000000)
+  fs.writeFileSync(
+    botonic_credentials_path,
+    JSON.stringify({ analytics: { anonymous_id } })
+  )
+}
+
+function readCredentials() {
+  if (!fs.existsSync(BOTONIC_CREDENTIALS_PATH)) {
+    initializeCredentials()
+  }
+  try {
+    credentials = JSON.parse(fs.readFileSync(BOTONIC_CREDENTIALS_PATH, 'utf8'))
+  } catch (e) {
+    if (fs.existsSync(BOTONIC_CREDENTIALS_PATH)) {
+      console.warn('Credentials could not be loaded', e)
+    }
+  }
+}
+
 function track(event, properties = {}) {
-  if (
-    analytics_enabled() &&
-    analytics &&
-    credentials &&
-    credentials.analytics
-  ) {
+  if (analyticsEnabled() && analytics && credentials && credentials.analytics) {
     properties = {
       ...properties,
       ...getSystemInformation(),
@@ -34,5 +62,10 @@ function track(event, properties = {}) {
 }
 
 const MODE = process.argv[2]
-if (MODE === 'serve') track('Served Botonic CLI')
-if (MODE === 'train') track('Trained with Botonic train')
+try {
+  readCredentials()
+  if (MODE === 'serve') track('Served Botonic CLI')
+  if (MODE === 'train') track('Trained with Botonic train')
+} catch (e) {
+  console.warn('Credentials could not be loaded', e)
+}
