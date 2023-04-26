079f361ac58e1dc8a5af1a6c6412713a14b4343e
Marc Rabat <marcrabat@gmail.com>
2023-04-26 23:59:11 +0200

    feat: working webviews but only with minify mode set to false

--- tutorial/esbuild-config.ts
+++ tutorial/esbuild-config.ts
@@ -4,6 +4,7 @@ import esbuild from 'esbuild'
 import { sassPlugin } from 'esbuild-sass-plugin'
 import inlineImage from 'esbuild-plugin-inline-image'
 import imageminPlugin from 'esbuild-plugin-imagemin'
+const { htmlPlugin } = require('@craftamap/esbuild-plugin-html')
 
 const nodeEntryPoint = './esbuild-entries/node-entry.js'
 const nodeOutputFile = './dist/bot.js'
@@ -56,19 +57,36 @@ const webchatBundle: esbuild.BuildOptions = {
 }
 
 const webviewsEntryPoint = './esbuild-entries/webviews-entry.js'
-const webviewsOutputFile = './dist/webviews/webviews.botonic.js'
+const webviewsOutputFile = './dist/webviews/webviews.js'
 
 const webviewsOutputDir = path.dirname(webviewsOutputFile)
 if (!fs.existsSync(webviewsOutputDir)) {
   fs.mkdirSync(webviewsOutputDir, { recursive: true })
 }
 
+const BOTONIC_PATH = path.resolve(
+  __dirname,
+  'node_modules',
+  '@botonic',
+  'react'
+)
+const TEMPLATES = {
+  WEBCHAT: 'webchat.template.html',
+  WEBVIEWS: 'webview.template.html',
+}
+const WEBVIEW_TEMPLATE_PATH = path.resolve(
+  BOTONIC_PATH,
+  'src',
+  TEMPLATES.WEBVIEWS
+)
+
 const webviewsBundle: esbuild.BuildOptions = {
-  entryPoints: [webviewsEntryPoint],
+  entryPoints: [{ in: webviewsEntryPoint, out: 'webviews' }],
   platform: 'browser',
-  outfile: webviewsOutputFile,
+  outdir: './dist/webviews',
   bundle: true,
-  minify: true,
+  // TODO: if minify set to true, webviews break
+  minify: false,
   sourcemap: false,
   format: 'iife',
   globalName: 'BotonicWebview',
@@ -76,12 +94,68 @@ const webviewsBundle: esbuild.BuildOptions = {
   loader: {
     '.js': 'jsx',
   },
+  metafile: true,
   define: { global: 'window' },
   assetNames: '../assets/[name]-[hash]',
-  plugins: [imageminPlugin(), inlineImage(), sassPlugin({ type: 'style' })],
+  plugins: [
+    imageminPlugin(),
+    inlineImage(),
+    sassPlugin({ type: 'style' }),
+    htmlPlugin({
+      files: [
+        {
+          entryPoints: [WEBVIEW_TEMPLATE_PATH],
+          filename: 'index.html',
+          htmlTemplate: `
+          <html>
+          <head>
+            <title></title>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <style>
+              html,
+              body,
+              #root {
+                width: 100%;
+                height: 100%;
+                margin: 0px;
+                padding: 0px;
+              }
+            </style>
+            <script defer="defer" src="webviews.js"></script>
+          </head>
+          <body>
+            <script>
+              ;(function (d, s, id) {
+                var js,
+                  fjs = d.getElementsByTagName(s)[0]
+                if (d.getElementById(id)) {
+                  return
+                }
+                js = d.createElement(s)
+                js.id = id
+                js.src = 'https://connect.facebook.net/en_US/messenger.Extensions.js'
+                fjs.parentNode.insertBefore(js, fjs)
+              })(document, 'script', 'Messenger')
+            </script>
+            <div id="root"></div>
+            <script>
+              document.addEventListener('DOMContentLoaded', function (event) {
+                BotonicWebview.render(document.getElementById('root'))
+              })
+            </script>
+          </body>
+        </html>                    
+          `,
+        },
+      ],
+    }),
+  ],
 }
 
-// Bundle the application using esbuild
 esbuild.build(nodeBundle).catch(() => process.exit(1))
 esbuild.build(webchatBundle).catch(() => process.exit(1))
-esbuild.build(webviewsBundle).catch(() => process.exit(1))
+esbuild.build(webviewsBundle).catch(e => {
+  console.log(e)
+  process.exit(1)
+})
