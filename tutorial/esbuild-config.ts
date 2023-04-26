3a1e09db8405bb8883c9da206ca27f61fc521ced
Marc Rabat <marcrabat@gmail.com>
2023-04-26 21:34:44 +0200

    feat: everything working with esbuild except staticAsset function and webviews

--- tutorial/esbuild-config.ts
+++ tutorial/esbuild-config.ts
@@ -3,17 +3,20 @@ import path from 'path'
 import esbuild from 'esbuild'
 import { sassPlugin } from 'esbuild-sass-plugin'
 import inlineImage from 'esbuild-plugin-inline-image'
+import imageminPlugin from 'esbuild-plugin-imagemin'
 
 const nodeEntryPoint = './esbuild-entries/node-entry.js'
-const outputFile = './dist/bot.js'
+const nodeOutputFile = './dist/bot.js'
 
-const outputDir = path.dirname(outputFile)
-if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true })
+const nodeOutputDir = path.dirname(nodeOutputFile)
+if (!fs.existsSync(nodeOutputDir)) {
+  fs.mkdirSync(nodeOutputDir, { recursive: true })
+}
 
 const nodeBundle: esbuild.BuildOptions = {
   entryPoints: [nodeEntryPoint],
   platform: 'node',
-  outfile: outputFile,
+  outfile: nodeOutputFile,
   bundle: true,
   minify: false,
   sourcemap: true,
@@ -22,25 +25,63 @@ const nodeBundle: esbuild.BuildOptions = {
   loader: {
     '.js': 'jsx',
   },
-  assetNames: 'assets/[name]',
+  assetNames: 'assets/[name]-[hash]',
   plugins: [inlineImage(), sassPlugin()],
 }
 
-const webpackBundle: esbuild.BuildOptions = {
-  entryPoints: [nodeEntryPoint],
+const webchatEntryPoint = './esbuild-entries/webchat-entry.js'
+const webchatOutputFile = './dist/webchat.botonic.js'
+
+const webchatOutputDir = path.dirname(webchatOutputFile)
+if (!fs.existsSync(webchatOutputDir)) {
+  fs.mkdirSync(webchatOutputDir, { recursive: true })
+}
+
+const webchatBundle: esbuild.BuildOptions = {
+  entryPoints: [webchatEntryPoint],
   platform: 'browser',
-  outfile: outputFile,
+  outfile: webchatOutputFile,
   bundle: true,
-  minify: false,
-  sourcemap: true,
-  format: 'esm',
+  minify: true,
+  sourcemap: false,
+  format: 'iife',
+  globalName: 'Botonic',
   external: ['esbuild'],
   loader: {
     '.js': 'jsx',
   },
-  assetNames: 'assets/[name]',
-  plugins: [inlineImage(), sassPlugin()],
+  define: { global: 'window' },
+  assetNames: 'assets/[name]-[hash]',
+  plugins: [imageminPlugin(), inlineImage(), sassPlugin({ type: 'style' })],
+}
+
+const webviewsEntryPoint = './esbuild-entries/webviews-entry.js'
+const webviewsOutputFile = './dist/webviews/webviews.botonic.js'
+
+const webviewsOutputDir = path.dirname(webviewsOutputFile)
+if (!fs.existsSync(webviewsOutputDir)) {
+  fs.mkdirSync(webviewsOutputDir, { recursive: true })
+}
+
+const webviewsBundle: esbuild.BuildOptions = {
+  entryPoints: [webviewsEntryPoint],
+  platform: 'browser',
+  outfile: webviewsOutputFile,
+  bundle: true,
+  minify: true,
+  sourcemap: false,
+  format: 'iife',
+  globalName: 'BotonicWebview',
+  external: ['esbuild'],
+  loader: {
+    '.js': 'jsx',
+  },
+  define: { global: 'window' },
+  assetNames: '../assets/[name]-[hash]',
+  plugins: [imageminPlugin(), inlineImage(), sassPlugin({ type: 'style' })],
 }
 
 // Bundle the application using esbuild
 esbuild.build(nodeBundle).catch(() => process.exit(1))
+esbuild.build(webchatBundle).catch(() => process.exit(1))
+esbuild.build(webviewsBundle).catch(() => process.exit(1))
